be4727be5d62cc6176ea3d6d94eaa631c449399d
unknown <pkumar1@tila.com>
2025-03-20 11:36:44 +0530

    backend initial commit

---
+++ lottery-back-end/src/main/java/com/project/lottery/LotteryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryBackEndApplication.class, args);
+	}
+
+}
---
+++ lottery-back-end/src/test/java/com/project/lottery/LotteryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
